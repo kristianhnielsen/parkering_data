0364a3c443973b0d6582532ec07c1d8795058cf8
Kristian <kristianhnielsen@protonmail.com>
2025-11-13 09:10:59 +0100

    clean up and date range splitting

--- webscraper/parkone.py
+++ webscraper/parkone.py
@@ -1,22 +1,28 @@
+from datetime import datetime, timezone
+import pandas as pd
 import requests
 from dotenv import load_dotenv
 import os
 from urllib.parse import urljoin
 
+from webscraper.utils import DateRange
 
-class ParkOne:
-    def __init__(self):
+
+class ParkOneAPI:
+    def __init__(self, date_range: DateRange):
         load_dotenv()
-        self._auth_token = os.getenv("PARKONE_API_TOKEN", "")
-        self.headers = {"authorization": self._auth_token}
-        self.base_url = "https://api.parkone.dk/v1"
+        self.date_range = date_range
+        self._auth_token = os.getenv("PARKONE_API_KEY", "")
+        self.headers = {
+            "content-type": "application/json",
+            "authorization": self._auth_token,
+        }
+        self.base_url = "https://api.parkone.dk/v1/"
         self.municipality = "vejle"
 
-    def get_by_vehicle(self, license_plate: str):
+    def get_all_parkings(self):
         """
-        Retrieves all the active parkings associated with a specific vehicle for the municipality.
-        Parameters:
-        license_plate: string
+        Retrieves all the active parkings for all municipalities.
 
         Response:
         parkingStartTime: string 				(Parking Start At)
@@ -25,15 +31,40 @@ class ParkOne:
         municipality: string 					(Operator)
         zone: string 						(Zone Name)
         """
-
-        endpoint = "/getActiveParkingsByVehicle"
+        endpoint = "Parkings/getAllParkings"
         url = urljoin(base=self.base_url, url=endpoint)
-        params = {"municipality": self.municipality, "vehicleRegId": license_plate}
+        df = pd.DataFrame()
 
-        response = requests.get(url, headers=self.headers, params=params)
-        response.raise_for_status()
+        # API docs specify no date ranges > 6 months. We split into 30 day intervals to be safe.
+        date_ranges = self.date_range.split(interval_days=30)
+        for date_range in date_ranges:
+            params = {
+                "municipality": self.municipality,
+                "startDate": self._dt_ms_format(date_range.start),
+                "endDate": self._dt_ms_format(date_range.end),
+            }
 
-        return response.json()
+            response = requests.get(url, headers=self.headers, params=params)
+            response.raise_for_status()
+            data = response.json()
+            df = pd.concat([df, pd.DataFrame(data)], ignore_index=True)
+            df["parkingStartTime"] = pd.to_datetime(df["parkingStartTime"])
+            df["parkingStopAt"] = pd.to_datetime(df["parkingStopAt"])
+
+        return df
+
+    def _dt_ms_format(self, dt: datetime) -> str:
+        return (
+            dt.astimezone(timezone.utc)
+            .isoformat(timespec="milliseconds")
+            .replace("+00:00", "Z")
+        )
 
 
-po = ParkOne()
+if __name__ == "__main__":
+    date_range = DateRange(start=datetime(2025, 8, 1), end=datetime.now())
+    parkone_api = ParkOneAPI(date_range=date_range)
+    response = parkone_api.get_all_parkings()
+    print(response.head())
+
+    print(response.dtypes)
